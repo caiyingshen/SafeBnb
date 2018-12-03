9702ad5b20136675c631f69da12d955ca3a1681c
caiyingshen <caiying.shen526@gmail.com>
2018-12-03 00:47:17 -0600

    add link to infowindow

--- frontend/src/components/MapPage.js
+++ frontend/src/components/MapPage.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { Link, Router, Route } from "react-router-dom";
+import ReactDOM from "react-dom";
+import { Link, BrowserRouter as Router, Route } from "react-router-dom";
 import { Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import { Button } from 'reactstrap';
 //import GoogleApiWrapper from './MapContainer'
 
 class MapPage extends Component {
@@ -10,11 +12,10 @@ class MapPage extends Component {
         showingInfoWindow: false,
         activeMarker: {},
         selectedPlace: {},
-        markers: [{ name: 'test1', lat: 42.3601, lng: -71.0589 },
-        { name: 'test2', lat: 41.35, lng: -70.0589 },
-        { name: 'test3', lat: 42.3601, lng: -70.0589 },
-        { name: 'test3', lat: 41.3601, lng: -72.0589 },
-        { name: 'test3', lat: 41.3601, lng: -71.0789 }]
+        markers: [{ name: 'test1', rank: "1", id: "123456", lat: 42.3601, lng: -71.0589 },
+        { name: 'test2', rank: 2, id: "123456", lat: 42.363, lng: -71.0560 },
+        { name: 'test3', rank: 3, id: "123456", lat: 42.365, lng: -71.053 }]
+
     }
   }
 
@@ -42,11 +43,27 @@ onMapClicked = () => {
 createMarkers = markers =>
   markers.map(m => (
           <Marker
-           name={m.name}
+           name={"#" + m.rank + ": " + m.name}
            position={{ lat: m.lat, lng: m.lng}}
            onClick={this.onMarkerClick}>
            </Marker>
   ))
+
+  onInfoWindowOpen(props, e) {
+    const button = (
+      <Button
+        onClick={e => {
+          window.location.href = "airbnb.com";
+        }}
+      >
+      Book!
+      </Button>
+    );
+    ReactDOM.render(
+      React.Children.only(button),
+      document.getElementById("iwc")
+    );
+  }
 
 
   render() {
@@ -60,20 +77,15 @@ createMarkers = markers =>
 
               {this.createMarkers(this.state.markers)}
 
-              <Marker
-                name="SOMA"
-                position={{ lat: 42.365, lng: -71.0590 }}
-                title="The marker`s title will appear as a tooltip."
-                onClick={this.onMarkerClick}
-              />
-
               <InfoWindow
                   marker={this.state.activeMarker}
                   onClose={this.onInfoWindowClose}
-                  visible={this.state.showingInfoWindow}>
-                  <div>
-                    <h1>{this.state.selectedPlace.name}</h1>
-                  </div>
+                  visible={this.state.showingInfoWindow}
+                  onOpen={e => {
+                          this.onInfoWindowOpen(this.props, e);
+                  }}>
+                  {this.state.selectedPlace.name}
+              <div id="iwc" />
                 </InfoWindow>
             </Map>
 
@@ -82,5 +94,5 @@ createMarkers = markers =>
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'API_KEY_HERE'
+  apiKey: 'AIzaSyAQDXd9PaHTytT46Gxa1rdyGN_g4pQW8bA'
 })(MapPage);
